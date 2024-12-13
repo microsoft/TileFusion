ab1c8de6754c40928b15e6be400e3e49538a0560
KuangjuX <18630816527@163.com>
2024-12-13 07:56:47 +0000

    Update

--- benchmarks/cpp/flashattention/cutlass_fa.cuh
+++ benchmarks/cpp/flashattention/cutlass_fa.cuh
@@ -23,12 +23,10 @@ using namespace cute;
 /// @tparam kTileQCol: Tile size of Q col.
 /// @tparam kTileKCol: Tile size of K col.
 /// @tparam kTileVCol: Tile size of V row.
-// template <typename Element_, const int L, const kDimK, const int kDimV,
-//           const int kTileQRow, const int kTileQCol, const int kTileKCol,
-//           const int kTileVCol, const int kThreads>
 template <typename Element_, const int kM, const int kN, const int kK,
           const int kP, const int kTM, const int kTN, const int kTK,
-          const int kTP, const int kThreads>
+          const int kTP, const int kThreads, const int SmemKAtom = 64,
+          const int kSwizzle = 3, typename Base = AccessBase<Element_>>
 struct FATraits : public Base {
     // Q: [kM, kK] --> [length, hidden_qk]
     // K: [kN, kK] --> [length, hidden_qk]
@@ -37,16 +35,55 @@ struct FATraits : public Base {
     // assert(kM == kN)
     using Element = Element_;
 
-    // declare global to shared memory copy layout.
+    // Declare global to shared memory copy layout.
     using GmemLayoutQ = Layout<Shape<Int<kTM>, Int<kTK>>, Stride<Int<kK>, _1>>;
     using GmemLayoutK = Layout<Shape<Int<kTN>, Int<kTK>>, Stride<Int<kK>, _1>>;
-    using GmemLayoutK = Layout<Shape<Int<kTP>, Int<kTN>>, Stride<Int<kN>, _1>>;
+    using GmemLayoutV = Layout<Shape<Int<kTP>, Int<kTN>>, Stride<Int<kN>, _1>>;
+    using GmemLayoutO = Layout<Shape<Int<kTM>, Int<kTP>>, Stride<Int<kP>, _1>>;
+
+    // Atom Shared Memory Block.
+    using SmemLayoutAtom = decltype(composition(
+        Swizzle<kSwizzle, 3, 3>{},
+        Layout<Shape<_8, Int<SmemKAtom>>, Stride<Int<SmemKAtom>, _1>>{}));
+
+    // Declare shared memory layout.
+    using SmemLayoutQ =
+        decltype(tile_to_shape(SmemLayoutAtom{}, Shape<Int<kTM>, Int<kTK>>{}));
+    using SmemLayoutK =
+        decltype(tile_to_shape(SmemLayoutAtom{}, Shape<Int<kTN>, Int<kTK>>{}));
+    ));
+    using SmemLayoutV =
+        decltype(tile_to_shape(SmemLayoutAtom{}, Shape<Int<kTP>, Int<kTN>>{}));
+    using SmemLayoutO =
+        decltype(tile_to_shape(SmemLayoutAtom{}, Shape<Int<kTM>, Int<kTP>>{}));
 
     constexpr int kWarps = kThreads / 32;
 
+    // Declare MMA Operation: [16, 8, 16] * [1, 2, 1] -> [16, 16, 16]
     using TiledMma =
         TiledMMA<MMA_Atom<SM80_16x8x16_F32F16F16F32_TN>,
                  Layout<Shape<Int<kWarps>, _1, _1>>, Layout<Shape<_1, _2, _1>>>;
+
+#ifdef CP_ASYNC_SM80_ENABLED
+    // for Ampere
+    using CopyInstG2S =
+        Copy_Atom<SM80_CP_ASYNC_CACHEGLOBAL<cute::uint128_t>, Element>;
+#else
+    using CopyInstG2S = Copy_Atom<DefaultCopy, Element>;
+#endif
+
+    // Declare Copy plan.
+    // Why this configuration?
+    using GmemCopyLayoutAtom =
+        Layout<Shape<Int<kThreads / (SmemKAtom / 8)>, Int<SmemKAtom / 8>>,
+               Stride<Int<SmemKAtom / 8>, _ 1>>;
+
+    using TiledCopyG2S = decltype(make_tiled_copy(
+        CopyInstG2S{}, GmemCopyLayoutAtom{}, Layout<Shape<_1, _8>>{}));
+
+    using TiledCopyS2G = decltype(make_tiled_copy(
+        Copy_Atom<DefaultCopy, Element>{}, GmemCopyLayoutAtom{},
+        Layout<Shape<_1, _8>>{}));
 };
 
 template <typename InType, typename AccType, typename KeTraits, const int kM,
@@ -68,6 +105,21 @@ __global__ void __launch_bounds__(Nthreads)
     const InType* V = dV + blockIdx.z * kP * kN + blockIdx.y * kTP * kN;
     InType* O =
         dO + blockIdx.z * kM * kP + blockIdx.x * (kTM * kP) + blockIdx.y * kTP;
+
+    InType* sQ_ptr = reinterpret_cast<Element*>(buf);
+    InType* sK_ptr = sQ_ptr + kTM * kTK;
+    InType* sV_ptr = sK_ptr + kTN * kTK;
+    InType* sO_ptr = sQ_ptr;
+
+    typename KeTraits::TiledMma mma;
+    typename KeTraits::TiledCopyG2S tiled_copy_g2s;
+
+    auto rQ = make_s2rA(sQ_ptr, typename KeTraits::SmemLayoutQ{}, mma);
+    auto rK = make_s2rA(sK_ptr, typename KeTraits::SmemLayoutK{}, mma);
+    auto acc1 = get_acc<kM, kN>(mma);
+
+    auto rV = make_s2rB(sV_ptr, typename KeTraits::SmemLayoutV{}, mma);
+    auto acc2 = get_acc<kM, kP>(mma);
 }
 
 }  // namespace cutlass_wrapper
