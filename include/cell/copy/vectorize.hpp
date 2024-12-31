f6bebc94cf5209f4b8d3a5868f4a803caa61bb33
KuangjuX <18630816527@163.com>
2024-12-30 22:25:44 -0800

    Add Vectorize struct brief.

--- include/cell/copy/vectorize.hpp
+++ include/cell/copy/vectorize.hpp
@@ -6,14 +6,28 @@
 #include "config.hpp"
 #include "cuda_utils.hpp"
 
+#include <cutlass/half.h>
+
 namespace tilefusion::cell::copy {
 
+/**
+ * @brief Vectorize a data type.
+ *
+ * @tparam Element Data type.
+ * @tparam kVecNums Number of vecotrized elements.
+ */
 template <typename Element, const int kVecNums>
 struct Vectorize {
     using UnVecType = Element;
     using VecType = Element;
     static constexpr int vectorize_nums = kVecNums;
 
+    /**
+     * @brief Copy data from unvectorized to vectorized.
+     *
+     * @param src Source data.
+     * @param dst Destination data.
+     */
     static __device__ inline void copy(const UnVecType* src, UnVecType* dst) {
         const VecType* src_vec = reinterpret_cast<const VecType*>(src);
         VecType* dst_vec = reinterpret_cast<VecType*>(dst);
@@ -26,8 +40,24 @@ struct Vectorize<__half, 2> {
     using UnVecType = __half;
     using VecType = __half2;
     static constexpr int vectorize_nums = 2;
+    static constexpr int vectorize_bits = 32;
 
     static __device__ inline void copy(const __half* src, __half* dst) {
+        const __half2* src_vec = reinterpret_cast<const __half2*>(src);
+        __half2* dst_vec = reinterpret_cast<__half2*>(dst);
+        *dst_vec = *src_vec;
+    }
+};
+
+template <>
+struct Vectorize<cutlass::half_t, 2> {
+    using UnVecType = cutlass::half_t;
+    using VecType = __half2;
+    static constexpr int vectorize_nums = 2;
+    static constexpr int vectorize_bits = 32;
+
+    static __device__ inline void copy(const cutlass::half_t* src,
+                                       cutlass::half_t* dst) {
         const __half2* src_vec = reinterpret_cast<const __half2*>(src);
         __half2* dst_vec = reinterpret_cast<__half2*>(dst);
         *dst_vec = *src_vec;
@@ -39,6 +69,7 @@ struct Vectorize<float, 2> {
     using UnVecType = float;
     using VecType = float2;
     static constexpr int vectorize_nums = 2;
+    static constexpr int vectorize_bits = 64;
 
     static __device__ inline void copy(const float* src, float* dst) {
         const float2* src_vec = reinterpret_cast<const float2*>(src);
