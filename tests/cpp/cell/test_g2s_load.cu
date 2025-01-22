46e1cddd44b4c26029ca3e8fd7630fec28fd6a75
KuangjuX <18630816527@163.com>
2025-01-22 08:09:10 +0000

    pass more float non-swizzled testcases.

--- tests/cpp/cell/test_g2s_load.cu
+++ tests/cpp/cell/test_g2s_load.cu
@@ -144,13 +144,12 @@ TEST(GlobalToSharedLoad, test_row_major_load) {
     run_test_row_major<__half, tl::RowMajor<2, 4>, 32, 256, true>();
     run_test_row_major<__half, tl::RowMajor<2, 4>, 64, 512, true>();
 
-    // run_test_row_major<float, tl::RowMajor<1, 1>, 16, 16>();
     run_test_row_major<float, tl::RowMajor<1, 1>, 8, 32, false>();
-    // run_test_row_major<float, tl::RowMajor<1, 2>, 32, 64>();
-    // run_test_row_major<float, tl::RowMajor<1, 4>, 32, 128>();
-    // run_test_row_major<float, tl::RowMajor<4, 1>, 192, 32>();
-    // run_test_row_major<float, tl::RowMajor<2, 2>, 64, 128>();
-    // run_test_row_major<float, tl::RowMajor<2, 4>, 96, 128>();
+    run_test_row_major<float, tl::RowMajor<1, 1>, 16, 64, false>();
+    run_test_row_major<float, tl::RowMajor<1, 4>, 16, 128, false>();
+    run_test_row_major<float, tl::RowMajor<4, 1>, 64, 32, false>();
+    run_test_row_major<float, tl::RowMajor<2, 2>, 32, 64, false>();
+    run_test_row_major<float, tl::RowMajor<2, 4>, 32, 128, false>();
 }
 
 TEST(GlobalToSharedLoad, test_col_major_load) {
